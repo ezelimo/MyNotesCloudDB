7a602bce9891bf4ea04c256d28068fcacce3b30b
ezelimo <79418403+ezelimo@users.noreply.github.com>
2021-12-21 19:51:56 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mynotesclouddb/Note.java
@@ -0,0 +1,37 @@
+package com.example.mynotesclouddb;
+
+import com.google.firebase.firestore.Exclude;
+
+import com.google.firebase.Timestamp;
+
+public class Note {
+
+    @Exclude
+    private String id;
+    private Timestamp date;
+    private String content;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Timestamp getDate() {
+        return date;
+    }
+
+    public void setDate(Timestamp date) {
+        this.date = date;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
